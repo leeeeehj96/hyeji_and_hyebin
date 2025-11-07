5678400e43626506d8858051080ec445f031887c
eunji <aggee212@gmail.com>
2025-11-07 17:34:53 +0900

    start

--- books/urls.py
+++ books/urls.py
@@ -6,12 +6,12 @@ app_name = 'books'  # URL reverse 네이밍용
 urlpatterns = [
     path('', views.index, name='index'),  # /books/
     path('create/', views.create, name='create'),
-    path('<int:pk>/', views.detail, name='detail'),
-    path('<int:pk>/update/', views.update, name='update'),
-    path('<int:pk>/delete/', views.delete, name='delete'),
+    # path('<int:pk>/', views.detail, name='detail'),
+    # path('<int:pk>/update/', views.update, name='update'),
+    # path('<int:pk>/delete/', views.delete, name='delete'),
 
-    path('<int:book_pk>/threads/create/', views.thread_create, name='thread_create'),
-    path('threads/<int:pk>/', views.thread_detail, name='thread_detail'),
-    path('threads/<int:pk>/update/', views.thread_update, name='thread_update'),
-    path('threads/<int:pk>/delete/', views.thread_delete, name='thread_delete'),
+    # path('<int:book_pk>/threads/create/', views.thread_create, name='thread_create'),
+    # path('threads/<int:pk>/', views.thread_detail, name='thread_detail'),
+    # path('threads/<int:pk>/update/', views.thread_update, name='thread_update'),
+    # path('threads/<int:pk>/delete/', views.thread_delete, name='thread_delete'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods, require_POST
+from .models import Book, Thread
+from .forms import BookForm, ThreadForm
 
-# Create your views here.
+@require_http_methods(['GET'])
+def index(request):
+    books = Book.objects.all()
+    context = {
+        'books': books,
+    }
+    return render(request, 'books/index.html', context)
